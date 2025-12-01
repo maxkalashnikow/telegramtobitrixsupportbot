3e326709b0bf008625a1173737cdab688401cd5f
Максим Калашников <kalashnikov.m@galereya.local>
2025-12-01 17:14:44 +0500

    Version 0.0.5

--- bot/settings.py
+++ bot/settings.py
@@ -5,4 +5,5 @@ load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 BITRIX_WEBHOOK_URL = os.getenv("BITRIX_WEBHOOK_URL")
-BITRIX_ENTITY_TYPE_ID = int(os.getenv("BITRIX_ENTITY_TYPE_ID", "0"))
+BITRIX_ENTITY_TYPE_ID = int(os.getenv("BITRIX_ENTITY_TYPE_ID"))
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # https://xxx.onrender.com/telegram/webhook
